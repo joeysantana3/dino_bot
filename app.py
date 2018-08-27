daaf39713a19a9bed5ce8af69701d93302c70d79
Joey Santana <joey@joeysantana.net>
2018-08-26 22:52:17 -0700

    added some comments

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import time
 from numpy import *
 import random
 
-# While game browser windows is maximized
+# While game browser window is helf screen to the right
+# 15" Retina MBP
 # www.trex-game.skipser.com
 # First coord, lower = to left
 # Second coord, lower = up
@@ -25,10 +26,11 @@ def press_space():
     print('Jump')
     pyautogui.keyUp('space')
 
-
+# Using global variable to increment function not good practice
 number = 1
 
 
+# Save an image of where PIL is looking. This is for setting the correct coords
 def box_image_check():
     global number
     box = (Coordintates.dino_loc[0]+1800, Coordintates.dino_loc[1]+400, Coordintates.dino_loc[0]+120+2000, Coordintates.dino_loc[1]+500)
@@ -37,6 +39,7 @@ def box_image_check():
     number += 1
 
 
+# Check box and return count
 def pixel_check():
     box = (Coordintates.dino_loc[0]+1800 , Coordintates.dino_loc[1]+400, Coordintates.dino_loc[0]+120+2000, Coordintates.dino_loc[1]+500)
     image = ImageGrab.grab(box)
@@ -47,9 +50,13 @@ def pixel_check():
     return a.sum()
 
 
+# This was just so I could orient where pyautogui was clicking
 def test_location():
     pyautogui.click(Coordintates.dino_loc)
 
+
+# This was so I could orient myself to the coordiantes pyautogui was using
+# Turns ou pyautogui and PIL use different coordinates
 # def test_box():
 #     # box = (Coordintates.dino_loc[0]+50, Coordintates.dino_loc[1], Coordintates.dino_loc[0]+40, Coordintates.dino_loc[1]+30)
 #     pyautogui.click(Coordintates.dino_loc[0]+120, Coordintates.dino_loc[1])
@@ -61,6 +68,7 @@ def test_location():
 #     pyautogui.click(Coordintates.dino_loc[0]+120+50, Coordintates.dino_loc[1]+30)
 
 
+# If ImageGrab result is between range, jump
 def auto_jump():
     if 33100 <= pixel_check() <= 33200:
         pass
@@ -69,6 +77,7 @@ def auto_jump():
         press_space()
 
 
+# Run the app
 restart_game()
 while True:
     # pixel_check()
